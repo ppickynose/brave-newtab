44a0109e2ce4eeb1ab56354055ae7833d11e38cd
Futur Fusionneur <fusionneur@yahoo.com>
2020-07-23 18:32:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 "use strict";
 
-(function(){
+(function () {
 
-	let dom = document.getElementById("bgimg");
-  	dom.style.backgroundColor =  'WhiteSmoke';
-	fetchImage();
+    let dom = document.getElementById("bgimg");
+    dom.style.backgroundColor = 'WhiteSmoke';
+    fetchImage();
 
-  	function fetchImage(){
-		   fetch('https://source.unsplash.com/random/1920x1080')
-		  .then(resp => resp)
-		  .then((imagelists) => {
-		  		let selectedImage = imagelists.url;
-		  	  	let dom = document.getElementById("bgimg");
-		  		dom.style.backgroundImage = `url(${selectedImage})`;
-		  })
-		   .catch(() => {
-		  		error();
-		  });
-  	}
+    function fetchImage() {
+        fetch('https://source.unsplash.com/random/1920x1080')
+        .then(resp => resp)
+        .then((imagelists) => {
+            let selectedImage = imagelists.url;
+            let dom = document.getElementById("bgimg");
+            dom.style.backgroundImage = `url(${selectedImage})`;
+        })
+        .catch(() => {
+            error();
+        });
+    }
 
-  	function error(){
-  		let dom = document.getElementById("bgimg");
-		dom.style.backgroundColor =  'WhiteSmoke';
-  	}
-
+    function error() {
+        let dom = document.getElementById("bgimg");
+        dom.style.backgroundColor = 'WhiteSmoke';
+    }
 
 })();
 
@@ -34,57 +33,55 @@
 
     function startTime() {
         var today = new Date(),
-            h = checkTime(today.getHours()),
-            m = checkTime(today.getMinutes()),
-            s = checkTime(today.getSeconds());
-            let time = h+":"+m;
-        		document.getElementById('time').innerHTML = time;
-        	 	setTimeout(function () {
+        h = checkTime(today.getHours()),
+        m = checkTime(today.getMinutes()),
+        s = checkTime(today.getSeconds());
+        let time = h + ":" + m;
+        document.getElementById('time').innerHTML = time;
+        setTimeout(function () {
             startTime()
         }, 500);
     }
     startTime();
 })();
 
-class Init{
+class Init {
 
-	constructor() {
-		this.dateDetails = null;
-	}
+    constructor() {
+        this.dateDetails = null;
+    }
 }
 
-class TabAction extends Init{
-	constructor(props) {
-	  super(props);
-	}
-	setDateDetails(){
-		this.dateDetails= getdateDetails();
-	}
+class TabAction extends Init {
+    constructor(props) {
+        super(props);
+    }
+    setDateDetails() {
+        this.dateDetails = getdateDetails();
+    }
 }
 
 let tab = new TabAction;
 tab.setDateDetails();
 insertinDom();
-function insertinDom(){
-	document.getElementById('date').innerHTML = `${tab.dateDetails.day}, ${tab.dateDetails.month} ${tab.dateDetails.date}`;
+function insertinDom() {
+    document.getElementById('date').innerHTML = `${tab.dateDetails.day}, ${tab.dateDetails.month} ${tab.dateDetails.date}`;
 }
 
+function getdateDetails() {
 
-function getdateDetails(){
-
-	var today = new Date();
-	var day = today.getDay();
-	var dd = today.getDate();
-	var mm = today.getMonth();
-	var yyyy = today.getFullYear();
-	var dL = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-	var mL = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	return {
-		day: dL[day],
-		month:  mL[mm],
-		date: dd,
-		year : yyyy
-	}
+    var today = new Date();
+    var day = today.getDay();
+    var dd = today.getDate();
+    var mm = today.getMonth();
+    var yyyy = today.getFullYear();
+    var dL = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var mL = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return {
+        day: dL[day],
+        month: mL[mm],
+        date: dd,
+        year: yyyy
+    }
 
 }
-
