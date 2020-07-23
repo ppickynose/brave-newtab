485e16d8784a8e2854f23b7cf08f9bdeff17549a
Futur Fusionneur <fusionneur@yahoo.com>
2020-07-23 18:33:44 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     // open a new tab after installing :)
     chrome.tabs.create({
         active: true
