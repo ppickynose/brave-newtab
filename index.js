c2fa243912796fd4ce22c659ed3db4502d710ad2
Futur Fusionneur <fusionneur@yahoo.com>
2020-09-08 05:31:14 +0300

    fadein on load + weather ready

--- index.js
+++ index.js
@@ -11,15 +11,15 @@
         .then(response => response)
         .then((imageObj) => {
 
-            var propValue;
+            /*             var propValue;
             for (var propName in imageObj) {
-                propValue = imageObj[propName]
-                console.log(propName, ":", propValue);
-            }
+            propValue = imageObj[propName]
+            console.log(propName, ":", propValue);
+            } */
 
-            var image = new Image(),
-            photoURL = imageObj.url;
-            console.log("photoURL: " + photoURL);
+            var image = new Image();
+            var photoURL = imageObj.url;
+            //console.log("photoURL: " + photoURL);
             image.onload = function () {
                 dom.style.backgroundImage = `url(${photoURL})`;
             };
@@ -94,3 +94,48 @@ function getdateDetails() {
     }
 
 }
+
+$.get("https://api.ipify.org?format=jsonp", function (response) {
+    //console.log(response); //{ip: "<ip>"}
+    var ip = response.ip;
+    //console.log(ip); //<ip>
+    $.get('https://ipapi.co/' + ip + '/latlong/', function (response) {
+		//console.log(response);
+        var latlong = response.split(',');
+        //console.log(latlong);
+        $.get('http://api.openweathermap.org/data/2.5/weather?lat=' + latlong[0] + '&lon=' + latlong[1] + '&appid=7e4dd03efbd4c382e324241cd5ab52ec' + '&units=metric', function (response) {
+            //console.log(response);
+			var realTemp = response.main.feels_like;
+			console.log(realTemp);
+			//{"coord":{"lon":<lon>,"lat":<lat>},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":292.77,"feels_like":294.58,"temp_min":291.15,"temp_max":294.26,"pressure":1018,"humidity":82},"visibility":10000,"wind":{"speed":0.5,"deg":0},"clouds":{"all":29},"dt":1599531594,"sys":{"type":1,"id":6911,"country":"<country>","sunrise":1599536809,"sunset":1599583197},"timezone":10800,"id":676742,"name":"<name>","cod":200}
+        })
+    })
+}, "jsonp");
+
+/*REF:
+// $. is an alias for jQuery
+
+//fetch image
+https://javascript.info/fetch
+
+//contentySecurityPolicy (manifest)
+https://developer.chrome.com/extensions/contentSecurityPolicy
+https://stackoverflow.com/questions/34950009/chrome-extension-refused-to-load-the-script-because-it-violates-the-following-c
+
+//libraries
+https://developers.google.com/speed/libraries
+https://jquery.com/download/
+
+//get IP
+https://www.ipify.org/ //use https://api.ipify.org?format=jsonp
+https://stackoverflow.com/questions/2067472/what-is-jsonp-and-why-was-it-created
+
+//location by ip
+https://ipapi.co/<ip>/latlong/
+https://ipinfo.io/ //not used (requires api key), but gives ip, city, country
+
+//weather by lat/lon
+https://api.openweathermap.org/data/2.5/weather?lat=44&lon=26&appid=<apiId>
+https://home.openweathermap.org/api_keys
+//
+*/
