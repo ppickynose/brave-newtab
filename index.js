c8aa6ceaf213ed9aec9cddb8edaa05852192eb40
Futur Fusionneur <fusionneur@yahoo.com>
2020-09-16 23:38:20 +0300

    improved timing
    await promise
    longer fade-in
    better positioning
    added localstorage for weather (update each 10 minutes)

--- index.js
+++ index.js
@@ -4,43 +4,54 @@
 
     let dom = document.getElementById("bgimg");
     dom.style.backgroundColor = 'WhiteSmoke';
-    fetchImage();
 
-    function fetchImage() {
-        //console.log("screen.resolution: " + screen.width + 'x' + screen.height);
-        fetch('https://source.unsplash.com/random/' + screen.width + 'x' + screen.height)
+    //console.log("screen.resolution: " + screen.width + 'x' + screen.height);
+    const promisedImage = fetch('https://source.unsplash.com/random/' + screen.width + 'x' + screen.height)
         .then(response => response)
         .then((imageObj) => {
+        console.log('in async');
+        return imageObj;
+    }).catch(() => {
+        error();
+    });
 
-            /*             var propValue;
-            for (var propName in imageObj) {
-            propValue = imageObj[propName]
-            console.log(propName, ":", propValue);
-            } */
+    /*var propValue;
+    for (var propName in imageObj) {
+    propValue = imageObj[propName]
+    console.log(propName, ":", propValue);
+    } */
 
-            var image = new Image();
-            var photoURL = imageObj.url;
-            //console.log("photoURL: " + photoURL);
-            image.onload = function () {
-                dom.style.backgroundImage = `url(${photoURL})`;
-            };
-            image.src = photoURL;
-        })
-        .catch(() => {
-            error();
-        });
-    }
+    /*ALTERNATIVE 1 (ORIGINAL)
+    var image = new Image();
+    var photoURL = imageObj.url;
+    //console.log("photoURL: " + photoURL);
+    image.onload = function () {
+    dom.style.backgroundImage = `url(${photoURL})`;
+    };
+    image.src = photoURL;
+     */
+
+
+    //ALTERNATIVE 3
+    (async function load() {
+        let photoObj = await promisedImage;
+		console.log("onload");
+        var photoURL = photoObj.url;
+        //console.log("photoURL: " + photoURL);
+        dom.style.backgroundImage = 'url(' + photoURL + ')';
+    })();
+
     function error() {
         let dom = document.getElementById("bgimg");
         dom.style.backgroundColor = 'WhiteSmoke';
     }
+
 })();
 
 (function () {
     function checkTime(i) {
         return (i < 10) ? "0" + i : i;
     }
-
     function startTime() {
         var today = new Date(),
         h = checkTime(today.getHours()),
@@ -56,7 +67,6 @@
 })();
 
 class Init {
-
     constructor() {
         this.dateDetails = null;
         this.weatherDetails = null;
@@ -82,7 +92,6 @@ function insertinDom() {
 }
 
 function getdateDetails() {
-
     var today = new Date();
     var day = today.getDay();
     var dd = today.getDate();
@@ -96,46 +105,130 @@ function getdateDetails() {
         date: dd,
         year: yyyy
     }
-
 }
 
 //____________________________________________________________________________
 
-/*
+//WEATHER & QUOTES
+
+var now = new Date();
+//console.log("time now: " + now.getTime());
+
+/* lat-long alternative
 $.get("https://api.ipify.org?format=jsonp", function (response) {
 console.log(response); //{ip: "<ip>"}
 var ip = response.ip;
 //console.log(ip); //<ip>
- */
+
 //$.get('https://ipapi.co/' + ip + '/latlong/', function (response) {
-$.get('https://ipapi.co/json', function (response) {
-    //console.log(response);
-    var city = response.city;
-    //console.log(city);
-    //var latlong = response.split(',');
-    //console.log(latlong);
-    //$.get('http://api.openweathermap.org/data/2.5/weather?lat=' + latlong[0] + '&lon=' + latlong[1] + '&appid=7e4dd03efbd4c382e324241cd5ab52ec' + '&units=metric', function (response) {
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7e4dd03efbd4c382e324241cd5ab52ec' + '&units=metric', function (response) {
-        //console.log(response);
-
-        var feelTemp = Math.round(response.main.feels_like);
-        //console.log("feelTemp: " + feelTemp);
-
-        var weatherDescription = response.weather[0].description;
-        //console.log(weatherDescription);
-
-        var humidity = response.main.humidity;
-        //console.log(humidity);
-
-        //console.log(feelTemp + "°C, " + weatherDescription + "..");
-        document.getElementById('weather').innerHTML = `${feelTemp}°C, ${weatherDescription}`;
-
-        //{"coord":{"lon":<lon>,"lat":<lat>},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":292.77,"feels_like":294.58,"temp_min":291.15,"temp_max":294.26,"pressure":1018,"humidity":82},"visibility":10000,"wind":{"speed":0.5,"deg":0},"clouds":{"all":29},"dt":1599531594,"sys":{"type":1,"id":6911,"country":"<country>","sunrise":1599536809,"sunset":1599583197},"timezone":10800,"id":676742,"name":"<name>","cod":200}
-    })
-})
-/*
+//var latlong = response.split(',');
+//console.log(latlong);
+//$.get('http://api.openweathermap.org/data/2.5/weather?lat=' + latlong[0] + '&lon=' + latlong[1] + '&appid=7e4dd03efbd4c382e324241cd5ab52ec' + '&units=metric', function (response) {
 }, "jsonp");
  */
+
+//Weather (updated each 10 minutes)
+if (typeof(Storage) !== "undefined") {
+    var expiry = localStorage.getItem("expiry");
+    //console.log("previous city expiry time: " + expiry);
+    //console.log("Web Storage support - OK");
+    if (now.getTime() < expiry) {
+        // Retrieving values from local storage and assign to variables
+        var feelTemp = localStorage.getItem("feelTemp");
+        var weatherDescription = localStorage.getItem("weatherDescription");
+        document.getElementById('weather').innerHTML = `${feelTemp}°C, ${weatherDescription}`;
+    } else { //local storage expired, get updated city
+        //console.log("previous city expired. Requesting new one");
+        $.get('https://ipapi.co/json', function (response) {
+            //console.log(response);
+            var city = response.city;
+            // Store value
+            localStorage.setItem("city", city);
+            //console.log(city);
+            const ttl = 10 * 60 * 1000; // 10 minutes to live (in milliseconds)
+            var expiry = now.getTime() + ttl;
+            //console.log(quoteExpiry);
+            localStorage.setItem("expiry", expiry);
+            $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=7e4dd03efbd4c382e324241cd5ab52ec' + '&units=metric', function (response) {
+                //console.log(response);
+                var feelTemp = Math.round(response.main.feels_like);
+                //console.log("feelTemp: " + feelTemp);
+                localStorage.setItem("feelTemp", feelTemp);
+                var weatherDescription = response.weather[0].description;
+                //console.log(weatherDescription);
+                localStorage.setItem("weatherDescription", weatherDescription);
+                //var humidity = response.main.humidity;
+                //console.log(humidity);
+                document.getElementById('weather').innerHTML = `${feelTemp}°C, ${weatherDescription}`;
+            })
+            //.done(function () {
+            //    alert("second success");
+            //})
+            //.fail(function () {
+            //    alert("error");
+            //})
+            //.always(function () {
+            //    alert("finished");
+            //});
+        })
+    }
+
+    //____________________________________________________________________________
+
+
+    //Quote of the day
+
+    var quoteExpiry = localStorage.getItem("quoteExpiry");
+    //console.log("previous quote expiry time: " + quoteExpiry);
+    //console.log("Web Storage support - OK");
+    if (now.getTime() < quoteExpiry) {
+        // Retrieve value from local storage and assign to variable
+        //localStorage.setItem("quote", "to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be "); //simulate local storage (to be commented in prod)
+        //localStorage.setItem("author", "Fusion"); //simulate local storage (to be commented in prod)
+        //console.log("Using quote from cached variable in local storage");
+        var quote = localStorage.getItem("quote");
+        var author = localStorage.getItem("author");
+        document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
+    } else { //local storage expired, get updated quote
+        const categories = ["inspire", "management", "life", "funny", "love", "art", "students"];
+        const random = Math.floor(Math.random() * categories.length);
+        const randomCategory = categories[random];
+        //console.log(randomCategory);
+        //console.log("previous quote expired. Requesting new one");
+        $.get('https://quotes.rest/qod?category=' + randomCategory, function (response) {
+            //console.log(response);
+            var quote = response.contents.quotes[0].quote;
+            var author = response.contents.quotes[0].author;
+            // Store value
+            localStorage.setItem("quote", quote);
+            localStorage.setItem("author", author);
+            const ttl = 60 * 60 * 1000; // 1hour time to live (in milliseconds)
+            //quotes.rest allows 10 calls per hour in free plan
+            var quoteExpiry = now.getTime() + ttl;
+            //console.log(quoteExpiry);
+            localStorage.setItem("quoteExpiry", quoteExpiry);
+            document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
+        })
+        //.done(function () {
+        //    alert("second success");
+        //})
+        .fail(function () {
+            const quote = "Understand the past, question the present, prepare for the future";
+            const author = "Fusionneur";
+            document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
+        })
+        //.always(function () {
+        //    alert("finished");
+        //});
+
+
+    }
+} else {
+    console.error("No Web Storage support");
+}
+
+//____________________________________________________________________________
+
 
 /*REF:
 // $. is an alias for jQuery
@@ -163,61 +256,9 @@ https://ipinfo.io/ //not used (requires api key), but gives ip, city, country
 https://api.openweathermap.org/data/2.5/weather?lat=44&lon=26&appid=<apiId>
 https://home.openweathermap.org/api_keys
 //
- */
 
-//____________________________________________________________________________
+//quotes
+//https://quotes.rest/
 
-
-//Quote of the day
-// Code for localStorage
-const now = new Date();
-//console.log("time now: " + now.getTime());
-if (typeof(Storage) !== "undefined") {
-    var quoteExpiry = localStorage.getItem("expiry");
-    //console.log("previous quote expiry time: " + quoteExpiry);
-    //console.log("Web Storage support - OK");
-    if (now.getTime() < quoteExpiry) {
-        // Retrieve value from local storage and assign to variable
-        //localStorage.setItem("quote", "to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be to be or not to be "); //simulate local storage (to be commented in prod)
-        //localStorage.setItem("author", "Fusion"); //simulate local storage (to be commented in prod)
-        //console.log("Using quote from cached variable in local storage");
-        var quote = localStorage.getItem("quote");
-        var author = localStorage.getItem("author");
-        document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
-    } else { //local storage expired, get updated quote
-        const categories = ["inspire", "management", "life", "funny", "love", "art", "students"];
-        const random = Math.floor(Math.random() * categories.length);
-        const randomCategory = categories[random];
-        //console.log(randomCategory);
-        $.get('https://quotes.rest/qod?category=' + randomCategory, function (response) {
-            //console.log(response);
-            //console.log("previous quote expired. Requesting new one");
-            var quote = response.contents.quotes[0].quote;
-            var author = response.contents.quotes[0].author;
-            // Store value
-            localStorage.setItem("quote", quote);
-            localStorage.setItem("author", author);
-            const ttl = 60 * 60 * 1000; // 1hour time to live (in milliseconds)
-            //quotes.rest allows 10 calls per hour in free plan
-            var expiry = now.getTime() + ttl;
-            //console.log(expiry);
-            localStorage.setItem("expiry", expiry);
-            document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
-        })
-        //.done(function () {
-        //    alert("second success");
-        //})
-        .fail(function () {
-            const quote = "Understand the past, question the present, prepare for the future";
-            const author = "Fusionneur";
-            document.getElementById('quote').innerHTML = `${quote}... <br> ${author}`;
-        })
-        //.always(function () {
-        //    alert("finished");
-        //});
-
-        //REF: https://api.jquery.com/jQuery.get/
-    }
-} else {
-    console.error("No Web Storage support");
-}
+//https://api.jquery.com/jQuery.get/
+*/
